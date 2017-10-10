05dad89f273c436752eeb581cad5133640466b52
Mark-43 <31340722+Zhuangwei-PENG@users.noreply.github.com>
2017-10-10 13:35:18 +0800

    Created cell with models in Profile ViewController

--- VolDuTemps/VolDuTemps/Classes/Views/Profile/TSProfileViewController.m
+++ VolDuTemps/VolDuTemps/Classes/Views/Profile/TSProfileViewController.m
@@ -11,29 +11,69 @@
 #import "TSSettingItem.h"
 
 @interface TSProfileViewController ()
-
+@property (nonatomic, strong) NSMutableArray *groups;
 @end
 
 @implementation TSProfileViewController
 
+static const NSString *cellId = @"staticCell";
+
+#pragma mark - Lazy instansitaion
+- (NSMutableArray *)groups{
+    if (!_groups) {
+        _groups = [NSMutableArray arrayWithCapacity:4];
+    }
+    return _groups;
+}
+
+#pragma mark - SepUp UI
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupUI{
+    [super setupUI];
+}
+- (void)setupTableView{
+    [super setupTableView];
+    
+    [self.myTableView registerClass:[UITableViewCell class] forCellReuseIdentifier:cellId];
+    TSSettingItem *setting = [TSSettingItem itemWithTitle:@"设置"];
+    TSSettingGroup *groupOne = [TSSettingGroup groupWithItems:@[setting]];
+    [self.groups addObject:groupOne];
+    
 }
 
-/*
+#pragma mark - DataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return [self.groups count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSArray *group = [self.groups[section] items];
+    return [group count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+    
+    TSSettingGroup *group = self.groups[indexPath.section];
+    TSSettingItem *item = group.items[indexPath.row];
+    cell.textLabel.text = [item title];
+    return cell;
+}
+#pragma mark - Delegate
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        return 100;
+    }
+    return 44;
+}
+
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+
 
 @end
